be055a6887512b425e0e1d07e13b5b3d45ddf180
TabeerJ <TabeerJ@github.com>
2017-12-25 12:50:33 +0500

    first commit

---
+++ Lecture 02/main.js
@@ -0,0 +1,86 @@
+// //(a2 + b2 + 2ab) solve this 
+// let a = prompt("Enter value for A");
+// let b = prompt("Enter value for B");
+
+// let solvea = parseInt (a * a);
+// let solveb = parseInt (b * b);
+
+// let solve2ab = parseInt (2 * a * b);
+
+// console.log(solvea + solveb + solve2ab); //Result = 9 
+
+
+
+
+//(a3 + 3a2b + 3ab2 + b3)
+// let a = prompt("Enter value for A");
+// let b = prompt("Enter value for b");
+
+// let solvea = parseInt(a * a * a);
+// let solve3a2b = parseInt(3 * a * a * b);
+// let solve3ab2 = parseInt(3 * a * b * b);
+// let solveb3 = parseInt(b * b * b);
+
+// console.log(solvea +solve3a2b + solve3ab2 + solveb3); //Result = 27 
+
+
+
+//(a2 + b2 - 4ab)
+// let c == 8;
+// let c= prompt("Enter value for c");
+// let d=  prompt("Enter value for d");
+
+// single if
+// if(c>d){
+//     console.log("c is greater than d");
+// }
+
+// if(c == d){
+//     console.log("value is match");
+// }
+
+// if(c>d){
+//     console.log("c is greater than d");
+// }
+// else{
+//     console.log("d is greater");
+// } 
+// let a = prompt("Enter value of A");
+
+// let b = prompt("Enter value of B");
+
+// let solvea = parseInt(a * a);
+// let solveb = parseInt(b * b);
+// let solve4ab = parseInt(4 * a * b);
+
+// console.log(solvea + solveb - solve4ab);    //Result = -3
+
+// 1. == 
+// 2. >
+// 3. <
+// 4. >=
+// 5. <=
+// 6. !=
+
+let f = prompt("Enter the value of Obtainedmarks");
+if(f==352){console.log("Bgrade");
+}
+if(f>375){
+    console.log("Agrade");
+}
+if(f<352){
+    console.log("Cgrade");
+} 
+if(f>=352){
+    console.log("Bgrade");
+}      
+if(f<=352){
+    console.log("Cgrade");
+}
+if(f!=200){
+    console.log("Fail");
+}
+ 
+
+
+
---
+++ Lecture 05/main.js
@@ -0,0 +1,44 @@
+// var a = 1; {
+//     var a = 2;
+// }
+// console.log(a);
+
+// var Dish1 = "Fish"
+// var Dish2 = "Chicken Karhai"
+// var Dish3 = "White Korma"
+// var Dish4 = "Mutton Karhai"
+// let Dish = prompt ('Enter the name of dish'); 
+// if(Dish === Dish1){
+//     console.log(800);
+// }else if(Dish === Dish2){
+//     console.log(400);
+// }
+// if(Dish === Dish3){
+//     console.log(850);
+// }   else if(Dish === Dish4)
+//     console.log(900);
+
+// CSGO
+var Pistol = 'Usp-s'
+var Sniper = 'Awp'
+var Gear = 'Kevlar'
+let Gun = prompt ('Enter the value of gun');
+if(Gun === "Pistol"){
+    console.log('Usp-s');
+}   else if (Gun === "Sniper"){
+    console.log('Awp');}
+        else if (Gun === "Gear"){
+            console.log('Kevlar');      
+ }
+var mouse1 = 'fire'
+var r = 'reload'
+let Attack = prompt ("Enter the value that will give damage to enemy")
+if(Attack === "mouse1"){
+    console.log('fire');
+} else if (Attack === "r"){
+    console.log('reload');
+}
+
+
+
+
